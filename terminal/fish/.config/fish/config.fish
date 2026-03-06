c3b103add69ff2948063dfb8a1d0740bd30bcb1b
imperaluna <rares.brezeanu.dev@gmail.com>
2026-03-06 10:16:25 -0500

    fix fish stealing tab

--- terminal/fish/.config/fish/config.fish
+++ terminal/fish/.config/fish/config.fish
@@ -2,6 +2,12 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 
+function fish_user_key_bindings
+    for mode in default insert
+        bind --mode $mode \t _fifc
+    end
+end
+
 starship init fish | source
 zoxide init --cmd cd fish | source
 set fish_greeting
